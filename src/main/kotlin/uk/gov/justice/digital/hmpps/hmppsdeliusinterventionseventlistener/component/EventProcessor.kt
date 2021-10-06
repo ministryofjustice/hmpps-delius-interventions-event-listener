d5b225bc452bf25a03aaa7d439cade3b42761848
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-10-06 19:57:14 +0100

    add feature flagging to disable this functionality until we are ready for it in production

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdeliusinterventionseventlistener/component/EventProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdeliusinterventionseventlistener/component/EventProcessor.kt
@@ -10,31 +10,40 @@ import uk.gov.justice.digital.hmpps.hmppsdeliusinterventionseventlistener.servic
 import java.net.URI
 
 private enum class EventType(val value: String) {
-  ACTION_PLAN_SUBMITTED("intervention.action-plan.submitted"),
+  ACTION_PLAN_SUBMITTED("intervention.action-plan.submitted");
+
+  companion object {
+    private val map = values().associateBy { it.value }
+    fun fromType(type: String) = map[type]
+  }
 }
 
 @Component
 class EventProcessor(
   private val interventionsApiService: InterventionsApiService,
   private val communityApiService: CommunityApiService,
+  private val featureFlags: FeatureFlags,
 ) {
   companion object : KLogging()
 
   fun process(event: InterventionsEvent) {
-    when (event.eventType) {
-      EventType.ACTION_PLAN_SUBMITTED.value -> processActionPlanSubmittedEvent(event)
-      else -> return
-    }
+    val eventType = EventType.fromType(event.eventType) ?: return
 
-    logger.debug("event processed successfully {}", kv("event", event))
+    logger.debug("processing event {}", kv("event", event))
+
+    when (eventType) {
+      EventType.ACTION_PLAN_SUBMITTED -> processActionPlanSubmittedEvent(event)
+    }
   }
 
   fun processActionPlanSubmittedEvent(event: InterventionsEvent) {
+    if (featureFlags.crs["notify-action-plan-submitted"] != true) return
+
     val actionPlan = interventionsApiService.get(URI(event.detailUrl), ActionPlan::class).block()
     val referral = interventionsApiService.getReferral(actionPlan.referralId).block()
     val intervention = interventionsApiService.getIntervention(referral.interventionId).block()
 
-    logger.debug("processing action plan submitted event {} {} {}", kv("actionPlan", actionPlan), kv("referral", referral), kv("intervention", intervention))
+    logger.debug("notifying nDelius of submitted action plan {} {} {}", kv("actionPlan", actionPlan), kv("referral", referral), kv("intervention", intervention))
     communityApiService.notifyActionPlanSubmitted(event.detailUrl, actionPlan, referral, intervention)
   }
 }
