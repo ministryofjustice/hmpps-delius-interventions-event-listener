94b32c52608f9e0af1091d26865c6227f939b35e
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-10-06 11:22:15 +0100

    Swap spring cloud for 'hmpps-spring-boot-sqs' to manage SQS queues.
    The motivation behind this change is:
    a) remain consistent with existing patterns within HMPPS
    b) utilize queue admin endpoints for interacting with DLQs
    c) contribute further enhancements back to the community

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdeliusinterventionseventlistener/component/SQSListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdeliusinterventionseventlistener/component/SQSListener.kt
@@ -1,19 +1,25 @@
 package uk.gov.justice.digital.hmpps.hmppsdeliusinterventionseventlistener.component
 
-import io.awspring.cloud.messaging.config.annotation.NotificationMessage
-import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy
-import io.awspring.cloud.messaging.listener.annotation.SqsListener
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KLogging
 import net.logstash.logback.argument.StructuredArguments
+import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsdeliusinterventionseventlistener.model.InterventionsEvent
 
 @Component
-class SQSListener {
+class SQSListener(private val objectMapper: ObjectMapper) {
   companion object : KLogging()
 
-  @SqsListener(value = ["\${cloud.aws.sqs.queues.interventions-events}"], deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-  fun listener(@NotificationMessage event: InterventionsEvent) {
+  @JmsListener(destination = "delius-interventions-events-queue", containerFactory = "hmppsQueueContainerFactoryProxy")
+  fun listener(rawMessage: String) {
+    val notification = objectMapper.readValue(rawMessage, NotificationMessage::class.java)
+    val event = objectMapper.readValue(notification.message, InterventionsEvent::class.java)
     logger.info("event received {}", StructuredArguments.kv("event", event))
   }
 }
+
+private data class NotificationMessage(
+  @JsonProperty("Message") val message: String
+)
