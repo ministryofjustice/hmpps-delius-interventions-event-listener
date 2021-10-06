ffc17be50bc839b01427f00012cd6d04feb63b17
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-10-06 10:27:05 +0100

    fix resource server configuration to allow secure endpoint access

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
   // web & security
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
   // aws
   implementation("io.awspring.cloud:spring-cloud-starter-aws-messaging")
