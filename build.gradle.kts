98434aadb1151b1b774c5612a6de12006ceef6a8
David Lantos <david.lantos@gmail.com>
2022-02-07 14:54:32 +0000

    Remove unnecessarily pinned dependencies
    General cleanup, to reduce redundancy

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  // bumps for security, until bumped in upstream
-  implementation("io.netty:netty-codec:4.1.73.Final")
-  implementation("ch.qos.logback:logback-classic:1.2.10") // CVE-2021-42550
-  implementation("ch.qos.logback:logback-core:1.2.10") // CVE-2021-42550
-  implementation("org.apache.logging.log4j:log4j-api:2.17.1") // CVE-2021-44228
-  implementation("org.apache.logging.log4j:log4j-to-slf4j:2.17.1") // CVE-2021-44228
 
   // aws
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.5")
